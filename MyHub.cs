7a1a97b605cf06239e379cce02e7c19187325d11
Jose Beltran <josebeltran@Joses-MacBook-Pro.local>
2018-04-06 04:12:19 +0200

    first commit. It should be done before :S

---
+++ MyHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreSignalR_alpha1
+{
+    public class MyHub : Hub
+    {
+        public Task BroadcastMessage(string message)
+        {            
+            return Clients.All.InvokeAsync("receivedOrderBroadcast", DateTime.Now.ToShortTimeString(), Context.ConnectionId, message);
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿//https://docs.microsoft.com/en-us/aspnet/signalr/overview/guide-to-the-api/hubs-api-guide-server
+
+
+function EverisGameConnection(){
+    //var transport = signalR.TransportType.WebSockets;
+    //let connection = new signalR.HubConnection('http://svqxxxxx:3000', { transport: transport });
+    let connection = new signalR.HubConnection('/hub');
+
+    connection.on('receivedOrderBroadcast', (timestamp, user, message) => {
+        $('#log').append(user + " (" + timestamp + ") - " + message + "<br/>");
+    });
+
+    connection.start().then(function() 
+                            {
+                                connection.invoke('BroadcastMessage', 'Success'); 
+                                event.preventDefault();
+                            }, function() {
+                                  console.log('ERROR'); 
+                            });
+}
